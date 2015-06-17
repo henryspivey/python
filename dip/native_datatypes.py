cd6679ca5844e8946d113d658f6dbe118f226df2
System Administrator <root@Henrys-MacBook-Pro.local>
2015-06-16 19:10:45 -0700

    Learned more about native datatypes

--- dip/native_datatypes.py
+++ dip/native_datatypes.py
@@ -81,4 +81,34 @@ userCount = 6
 # use
 print "Users connected: %d" % (userCount,)
 
+# formatting numbers
+print "Today's stock price: %f" % 50.4625
+print "Today's stock price: %.2f"% 50.4625
+print "change since yesterday: %+.2f" %1.5
 
+
+# mapping lists
+li = [1,9,8,4]
+print [elem*2 for elem in li]
+
+
+params = {"server": "mpilgrim", "database":"master",
+"uid":"sa", "pwd":"secret"}
+print params.keys()
+print params.values()
+print params.items()
+print ["%s=%s" %(i,j) for (i,j) in params.items()]
+
+
+
+#joining lists and splitting strings
+print ";".join(["%s=%s" %(i,j) for (i,j) in params.items()])
+# ; is an object and I am calling its join method
+# join method joins the elements of the list into a single string
+
+
+#splitting a string
+li = ["server=mpilgrim", "uid=sa", "database=master", "pwd=secret"]
+s = ";".join(li)
+print s
+print s.split(";")
